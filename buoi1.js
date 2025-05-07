5436fd06fe44f38d7fba773494791f2c790bfd77
seiber205 <canhcutgamo@gmail.com>
2025-05-08 02:48:44 +0700

    Buoi 1: Exercise

--- buoi1.js
+++ buoi1.js
@@ -1,6 +1,11 @@
-async function getData() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-    const data = await response.json();
-    return data.title;
-  }
-  getData().then(title => console.log(title));
+const multiply= (a, b) => a*b ;
+console.log(multiply(5, 4))
+
+async function getPostTitle(id) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await response.json();
+  return data.title;
+}
+getPostTitle(2).then(title => console.log(title));
+
+
